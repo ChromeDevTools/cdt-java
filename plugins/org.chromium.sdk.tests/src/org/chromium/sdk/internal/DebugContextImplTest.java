26d4df5f3e034020a394ad0f60c4eccc7c9e1acc
peter.rybin <peter.rybin@gmail.com>
2009-08-05 20:12:31 +0000

    Fix race condition in tests
    Review URL: http://codereview.chromium.org/160652

--- plugins/org.chromium.sdk.tests/src/org/chromium/sdk/internal/DebugContextImplTest.java
+++ plugins/org.chromium.sdk.tests/src/org/chromium/sdk/internal/DebugContextImplTest.java
@@ -31,28 +31,33 @@ public class DebugContextImplTest extends AbstractAttachedTest<FakeConnection>{
    */
   @Test(timeout = 5000)
   public void checkContextIsInvalidatedOnContinue() throws Exception {
-    final CountDownLatch latch = new CountDownLatch(1);
     final Breakpoint[] bp = new Breakpoint[1];
     final String[] failure = new String[1];
-    browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
-        new BreakpointCallback() {
-
-          public void failure(String errorMessage) {
-            failure[0] = errorMessage == null ? "" : errorMessage;
-            latch.countDown();
-          }
-
-          public void success(Breakpoint breakpoint) {
-            bp[0] = breakpoint;
-            latch.countDown();
-          }
-        });
-    latch.await();
+    {
+      final CountDownLatch latch = new CountDownLatch(1);
+      browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
+          new BreakpointCallback() {
+  
+            public void failure(String errorMessage) {
+              failure[0] = errorMessage == null ? "" : errorMessage;
+              latch.countDown();
+            }
+  
+            public void success(Breakpoint breakpoint) {
+              bp[0] = breakpoint;
+              latch.countDown();
+            }
+          });
+      latch.await();
+    }
     assertNull("Failed to set a breakpoint: " + failure[0], failure[0]);
     assertNotNull("Breakpoint not set", bp[0]);
 
-    messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
-    waitForSuspend();
+    {
+      CountDownLatch latch = expectSuspend();
+      messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
+      latch.await();
+    }
     Collection<JsVariableImpl> variables = suspendContext.getCallFrames().get(0).getVariables();
 
     // This call invalidates the debug context for the "lookup" operation that is invoked
@@ -70,28 +75,33 @@ public class DebugContextImplTest extends AbstractAttachedTest<FakeConnection>{
    */
   @Test(timeout = 5000)
   public void checkContextIsValidOffHand() throws Exception {
-    final CountDownLatch latch = new CountDownLatch(1);
     final Breakpoint[] bp = new Breakpoint[1];
     final String[] failure = new String[1];
-    browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
-        new BreakpointCallback() {
-
-          public void failure(String errorMessage) {
-            failure[0] = errorMessage == null ? "" : errorMessage;
-            latch.countDown();
-          }
-
-          public void success(Breakpoint breakpoint) {
-            bp[0] = breakpoint;
-            latch.countDown();
-          }
-        });
-    latch.await();
+    {
+      final CountDownLatch latch = new CountDownLatch(1);
+      browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
+          new BreakpointCallback() {
+  
+            public void failure(String errorMessage) {
+              failure[0] = errorMessage == null ? "" : errorMessage;
+              latch.countDown();
+            }
+  
+            public void success(Breakpoint breakpoint) {
+              bp[0] = breakpoint;
+              latch.countDown();
+            }
+          });
+      latch.await();
+    }
     assertNull("Failed to set a breakpoint: " + failure[0], failure[0]);
     assertNotNull("Breakpoint not set", bp[0]);
 
-    messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
-    waitForSuspend();
+    {
+      CountDownLatch latch = expectSuspend();
+      messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
+      latch.await();
+    }
     Collection<JsVariableImpl> variables = suspendContext.getCallFrames().get(0).getVariables();
 
     JsObjectImpl jsObject = variables.iterator().next().getValue().asObject();
--- plugins/org.chromium.sdk.tests/src/org/chromium/sdk/internal/DebugEventListenerTest.java
+++ plugins/org.chromium.sdk.tests/src/org/chromium/sdk/internal/DebugEventListenerTest.java
@@ -33,28 +33,34 @@ public class DebugEventListenerTest extends AbstractAttachedTest<FakeConnection>
 
   @Test(timeout = 5000)
   public void testSuspendResume() throws Exception {
-    final CountDownLatch latch = new CountDownLatch(1);
     final Breakpoint[] bp = new Breakpoint[1];
     final String[] failure = new String[1];
-    browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
-        new BreakpointCallback() {
-
-          public void failure(String errorMessage) {
-            failure[0] = errorMessage == null ? "" : errorMessage;
-            latch.countDown();
-          }
-
-          public void success(Breakpoint breakpoint) {
-            bp[0] = breakpoint;
-            latch.countDown();
-          }
-        });
-    latch.await();
+    {
+      final CountDownLatch latch = new CountDownLatch(1);
+      browserTab.setBreakpoint(Breakpoint.Type.SCRIPT_NAME, "file:///C:/1.js", 18, 3, true, null, 0,
+          new BreakpointCallback() {
+  
+            public void failure(String errorMessage) {
+              failure[0] = errorMessage == null ? "" : errorMessage;
+              latch.countDown();
+            }
+  
+            public void success(Breakpoint breakpoint) {
+              bp[0] = breakpoint;
+              latch.countDown();
+            }
+          });
+      latch.await();
+    }
     assertNull("Failed to set a breakpoint: " + failure[0], failure[0]);
     assertNotNull("Breakpoint not set", bp[0]);
 
-    messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
-    waitForSuspend();
+    {
+      CountDownLatch latch = expectSuspend();
+      messageResponder.hitBreakpoints(Collections.singleton(bp[0].getId()));
+      latch.await();
+    }
+    
     assertNotNull("suspended() not invoked after the break event", suspendContext);
     Collection<Breakpoint> breakpointsHit = suspendContext.getBreakpointsHit();
     assertEquals(1, breakpointsHit.size());
@@ -63,8 +69,11 @@ public class DebugEventListenerTest extends AbstractAttachedTest<FakeConnection>
 
     resume();
 
-    messageResponder.hitBreakpoints(Collections.<Long> emptySet());
-    waitForSuspend();
+    {
+      CountDownLatch latch = expectSuspend();
+      messageResponder.hitBreakpoints(Collections.<Long> emptySet());
+      latch.await();
+    }
     assertNotNull("suspended() not invoked after the break event", suspendContext);
     breakpointsHit = suspendContext.getBreakpointsHit();
     assertTrue(breakpointsHit.isEmpty());
