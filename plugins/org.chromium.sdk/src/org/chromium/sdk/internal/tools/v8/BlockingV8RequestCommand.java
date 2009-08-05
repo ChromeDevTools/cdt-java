9dca5120fa293cbbf20cf90a9fb5f1c11e78d945
peter.rybin <peter.rybin@gmail.com>
2009-08-05 17:33:48 +0000

    generic V8 code is being extracted from V8DebuggerToolHandler into a separate V8CommandProcessor class with V8CommandOutput as additional intermediate interface.
    Other changes seems to be update references changes.
    
    Review URL: http://codereview.chromium.org/160648

--- plugins/org.chromium.sdk/src/org/chromium/sdk/internal/tools/v8/BlockingV8RequestCommand.java
+++ plugins/org.chromium.sdk/src/org/chromium/sdk/internal/tools/v8/BlockingV8RequestCommand.java
@@ -4,22 +4,20 @@
 
 package org.chromium.sdk.internal.tools.v8;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-
-import org.chromium.sdk.internal.BrowserTabImpl;
 import org.chromium.sdk.internal.tools.v8.request.DebuggerMessage;
 import org.json.simple.JSONObject;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
  * An implementation of
- * {@link V8DebuggerToolHandler#sendV8CommandBlocking(
- * DebuggerMessage, org.chromium.sdk.internal.BrowserTabImpl.V8HandlerCallback)}
+ * {@link V8CommandProcessor#sendV8CommandBlocking(
+ * DebuggerMessage, V8CommandProcessor.V8HandlerCallback)}
  * which blocks until the response has been received and callback (if any) has
  * been invoked.
  */
 class BlockingV8RequestCommand implements Runnable {
-
   /**
    * The waiting time for an operation. An operation is considered timed out if
    * it does not complete within this period.
@@ -29,7 +27,7 @@ class BlockingV8RequestCommand implements Runnable {
   /**
    * The handler to use while sending a message.
    */
-  private final V8DebuggerToolHandler handler;
+  private final V8CommandProcessor v8CommandProcessor;
 
   /**
    * The message to send.
@@ -39,7 +37,7 @@ class BlockingV8RequestCommand implements Runnable {
   /**
    * The callback to invoke once the result is available.
    */
-  private final BrowserTabImpl.V8HandlerCallback v8HandlerCallback;
+  private final V8CommandProcessor.V8HandlerCallback v8HandlerCallback;
 
   /**
    * Whether to send the "evaluate_javascript" command so that the primary
@@ -50,16 +48,16 @@ class BlockingV8RequestCommand implements Runnable {
   private Exception exception = null;
 
   /**
-   * @param handler the V8DebuggerToolHandler instance
+   * @param v8CommandProcessor the V8CommandProcessor instance
    * @param message to send
    * @param immediate whether to evaluate JavaScript so that the request is
    *        handled immediately (should always be the case if potentially
    *        calling a command not on a breakpoint)
    * @param v8HandlerCallback to invoke on reply from the browser
    */
-  BlockingV8RequestCommand(V8DebuggerToolHandler handler, DebuggerMessage message,
-      boolean immediate, BrowserTabImpl.V8HandlerCallback v8HandlerCallback) {
-    this.handler = handler;
+  BlockingV8RequestCommand(V8CommandProcessor v8CommandProcessor, DebuggerMessage message,
+      boolean immediate, V8CommandProcessor.V8HandlerCallback v8HandlerCallback) {
+    this.v8CommandProcessor = v8CommandProcessor;
     this.message = message;
     this.v8HandlerCallback = v8HandlerCallback;
     this.isImmediate = immediate;
@@ -69,7 +67,8 @@ class BlockingV8RequestCommand implements Runnable {
     Exception[] result = new Exception[1];
     try {
       final Semaphore sem = new Semaphore(0);
-      BrowserTabImpl.V8HandlerCallback commandCallback = new BrowserTabImpl.V8HandlerCallback() {
+      V8CommandProcessor.V8HandlerCallback commandCallback =
+          new V8CommandProcessor.V8HandlerCallback() {
         public void messageReceived(JSONObject response) {
           try {
             if (v8HandlerCallback != null) {
@@ -90,7 +89,7 @@ class BlockingV8RequestCommand implements Runnable {
           }
         }
       };
-      handler.sendV8Command(message, isImmediate, commandCallback);
+      v8CommandProcessor.sendV8Command(message, isImmediate, commandCallback);
       if (!sem.tryAcquire(OPERATION_TIMEOUT_MS, TimeUnit.MILLISECONDS)) {
         commandCallback.failure("Timed out");
       }
--- plugins/org.chromium.sdk/src/org/chromium/sdk/internal/tools/v8/V8DebuggerToolHandler.java
+++ plugins/org.chromium.sdk/src/org/chromium/sdk/internal/tools/v8/V8DebuggerToolHandler.java
@@ -4,16 +4,6 @@
 
 package org.chromium.sdk.internal.tools.v8;
 
-import java.text.MessageFormat;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.chromium.sdk.DebugEventListener;
 import org.chromium.sdk.internal.BrowserImpl;
 import org.chromium.sdk.internal.BrowserTabImpl;
@@ -23,17 +13,16 @@ import org.chromium.sdk.internal.MessageFactory;
 import org.chromium.sdk.internal.Result;
 import org.chromium.sdk.internal.tools.ChromeDevToolsProtocol;
 import org.chromium.sdk.internal.tools.ToolHandler;
-import org.chromium.sdk.internal.tools.v8.processor.AfterCompileProcessor;
-import org.chromium.sdk.internal.tools.v8.processor.BacktraceProcessor;
-import org.chromium.sdk.internal.tools.v8.processor.BreakpointProcessor;
-import org.chromium.sdk.internal.tools.v8.processor.ContinueProcessor;
-import org.chromium.sdk.internal.tools.v8.processor.V8ResponseCallback;
 import org.chromium.sdk.internal.tools.v8.request.DebuggerMessage;
-import org.chromium.sdk.internal.tools.v8.request.V8MessageType;
 import org.chromium.sdk.internal.transport.Connection;
 import org.chromium.sdk.internal.transport.Message;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Handles the interaction with the "V8Debugger" tool.
@@ -70,50 +59,8 @@ public class V8DebuggerToolHandler implements ToolHandler {
     void resultReceived(Result result);
   }
 
-  private static class CallbackEntry {
-    final BrowserTabImpl.V8HandlerCallback v8HandlerCallback;
-
-    final long commitMillis;
-
-    final Semaphore semaphore;
-
-    public CallbackEntry(BrowserTabImpl.V8HandlerCallback v8HandlerCallback, long commitMillis) {
-      this(v8HandlerCallback, commitMillis, null);
-    }
-
-    public CallbackEntry(BrowserTabImpl.V8HandlerCallback v8HandlerCallback, long commitMillis,
-        Semaphore semaphore) {
-      this.v8HandlerCallback = v8HandlerCallback;
-      this.commitMillis = commitMillis;
-      this.semaphore = semaphore;
-    }
-  }
-
   /** The class logger. */
   private static final Logger LOGGER = Logger.getLogger(V8DebuggerToolHandler.class.getName());
-
-  private static final EnumSet<DebuggerCommand> CONTEXT_DEPENDENT_COMMANDS = EnumSet.of(
-      DebuggerCommand.LOOKUP,
-      DebuggerCommand.EVALUATE,
-      DebuggerCommand.FRAME);
-
-  /**
-   * The callbacks to invoke when the responses arrive.
-   */
-  private final Map<Integer, CallbackEntry> seqToV8Callbacks =
-      new HashMap<Integer, CallbackEntry>();
-
-  /**
-   * The handlers that should be invoked when certain command responses arrive.
-   */
-  private final Map<DebuggerCommand, V8ResponseCallback> commandToHandlerMap =
-      new HashMap<DebuggerCommand, V8ResponseCallback>();
-
-  /** The breakpoint processor. */
-  private final BreakpointProcessor bpp;
-
-  /** The "afterCompile" event processor. */
-  private final AfterCompileProcessor afterCompileProcessor;
 
   /** The host BrowserImpl instance. */
   private final BrowserImpl browserImpl;
@@ -131,7 +78,7 @@ public class V8DebuggerToolHandler implements ToolHandler {
 
   private ResultAwareCallback detachCallback;
 
-  private final Object sendLock = new Object();
+  private final V8CommandProcessor v8CommandProcessor;
 
   /**
    * A no-op JavaScript to evaluate.
@@ -141,28 +88,12 @@ public class V8DebuggerToolHandler implements ToolHandler {
   public V8DebuggerToolHandler(BrowserImpl browserImpl, DebugContextImpl context) {
     this.browserImpl = browserImpl;
     this.context = context;
-    this.bpp = new BreakpointProcessor(context);
-    this.afterCompileProcessor = new AfterCompileProcessor(context);
-
-    commandToHandlerMap.put(DebuggerCommand.CHANGEBREAKPOINT, bpp);
-    commandToHandlerMap.put(DebuggerCommand.SETBREAKPOINT, bpp);
-    commandToHandlerMap.put(DebuggerCommand.CLEARBREAKPOINT, bpp);
-    commandToHandlerMap.put(DebuggerCommand.BREAK /* event */, bpp);
-    commandToHandlerMap.put(DebuggerCommand.EXCEPTION /* event */, bpp);
-
-    commandToHandlerMap.put(DebuggerCommand.AFTER_COMPILE /* event */, afterCompileProcessor);
-
-    commandToHandlerMap.put(DebuggerCommand.BACKTRACE, new BacktraceProcessor(context));
-
-    commandToHandlerMap.put(DebuggerCommand.CONTINUE, new ContinueProcessor(context));
+    ChromeDevToolMessageOutput messageOutput = new ChromeDevToolMessageOutput(context.getTab());
+    this.v8CommandProcessor = new V8CommandProcessor(messageOutput, context);
   }
 
   public DebugEventListener getDebugEventListener() {
     return context.getTab().getDebugEventListener();
-  }
-
-  public BreakpointProcessor getBreakpointProcessor() {
-    return bpp;
   }
 
   public void handleMessage(final Message message) {
@@ -206,7 +137,7 @@ public class V8DebuggerToolHandler implements ToolHandler {
     synchronized (fieldAccessLock) {
       isAttached = false;
     }
-    removeAllCallbacks();
+    v8CommandProcessor.removeAllCallbacks();
     DebugEventListener debugEventListener = getDebugEventListener();
     if (debugEventListener != null) {
       debugEventListener.disconnected();
@@ -230,48 +161,10 @@ public class V8DebuggerToolHandler implements ToolHandler {
     }
   }
 
-  public Exception sendV8CommandBlocking(
-      DebuggerMessage message, BrowserTabImpl.V8HandlerCallback v8HandlerCallback) {
-    BlockingV8RequestCommand command =
-        new BlockingV8RequestCommand(this, message, true, v8HandlerCallback);
-    command.run();
-    return command.getException();
-  }
 
   public void sendV8Command(DebuggerMessage message,
-      boolean isImmediate, BrowserTabImpl.V8HandlerCallback v8HandlerCallback) {
-    synchronized (sendLock) {
-      if (isMessageContextDependent(message) && !message.getToken().isValid()) {
-        if (v8HandlerCallback != null) {
-          v8HandlerCallback.failure("Invalid context");
-        }
-        return;
-      }
-      if (DebuggerCommand.CONTINUE.value.equals(message.getCommand())) {
-        message.getToken().invalidate();
-      }
-      if (v8HandlerCallback != null) {
-        seqToV8Callbacks.put(message.getSeq(), new CallbackEntry(v8HandlerCallback,
-            getCurrentMillis()));
-      }
-      try {
-        String destination = String.valueOf(getAttachedTab());
-        getConnection().send(
-            MessageFactory.debuggerCommand(
-                destination,
-                JsonUtil.streamAwareToJson(message)));
-        if (isImmediate) {
-          getConnection().send(
-              MessageFactory.evaluateJavascript(destination, JAVASCRIPT_VOID));
-        }
-      } catch (RuntimeException e) {
-        if (v8HandlerCallback != null) {
-          v8HandlerCallback.failure(e.getMessage());
-          seqToV8Callbacks.remove(message.getSeq());
-        }
-        throw e;
-      }
-    }
+      boolean isImmediate, V8CommandProcessor.V8HandlerCallback v8HandlerCallback) {
+    v8CommandProcessor.sendV8Command(message, isImmediate, v8HandlerCallback);
   }
 
   /**
@@ -342,50 +235,6 @@ public class V8DebuggerToolHandler implements ToolHandler {
     return output[0];
   }
 
-  /**
-   * @return milliseconds since the epoch
-   */
-  protected long getCurrentMillis() {
-    return System.currentTimeMillis();
-  }
-
-  /**
-   * @param type response type ("response" or "event")
-   * @param response from the V8 VM debugger
-   */
-  private void handleResponse(V8MessageType type, final JSONObject response) {
-    String commandString = JsonUtil.getAsString(response, V8MessageType.RESPONSE == type
-        ? V8Protocol.KEY_COMMAND
-        : V8Protocol.KEY_EVENT);
-    DebuggerCommand command = DebuggerCommand.forString(commandString);
-    if (command == null) {
-      LOGGER.log(Level.WARNING,
-          MessageFormat.format("Unknown command in V8 debugger reply JSON: {0}", commandString));
-      return;
-    }
-    final V8ResponseCallback handler = commandToHandlerMap.get(command);
-    if (handler == null) {
-      return;
-    }
-    handler.messageReceived(response);
-  }
-
-  private void checkNull(Object object, String message) {
-    if (object == null) {
-      throw new IllegalArgumentException(message);
-    }
-  }
-
-  private void removeAllCallbacks() {
-    for (Iterator<CallbackEntry> it = seqToV8Callbacks.values().iterator(); it.hasNext();) {
-      CallbackEntry entry = it.next();
-      if (entry.semaphore != null) {
-        entry.semaphore.release();
-      }
-      it.remove();
-    }
-  }
-
   private void processClosed(JSONObject json) {
     synchronized (fieldAccessLock) {
       if (detachCallback != null) {
@@ -454,39 +303,9 @@ public class V8DebuggerToolHandler implements ToolHandler {
   }
 
   private void processDebuggerCommand(JSONObject json) {
-    final JSONObject v8Json = JsonUtil.getAsJSON(json, ChromeDevToolsProtocol.DATA.key);
-    checkNull(v8Json, "'data' field not found");
-    V8MessageType type = V8MessageType.forString(JsonUtil.getAsString(v8Json, V8Protocol.KEY_TYPE));
-    handleResponse(type, v8Json);
-    if (V8MessageType.RESPONSE == type) {
-      final int requestSeq = JsonUtil.getAsLong(v8Json, V8Protocol.KEY_REQSEQ).intValue();
-      checkNull(requestSeq, "Could not read 'request_seq' from debugger reply");
-      final CallbackEntry callbackEntry = seqToV8Callbacks.remove(requestSeq);
-      if (callbackEntry != null) {
-        LOGGER.log(Level.INFO,
-            MessageFormat.format(
-                "Request-response roundtrip: {0}ms",
-                getCurrentMillis() - callbackEntry.commitMillis));
-        Thread t = new Thread(new Runnable() {
-          public void run() {
-            try {
-              if (callbackEntry.v8HandlerCallback != null) {
-                LOGGER.log(
-                    Level.INFO, "Notified debugger command callback, request_seq=" + requestSeq);
-                callbackEntry.v8HandlerCallback.messageReceived(v8Json);
-              }
-            } finally {
-              // Note that the semaphore is released AFTER the handleResponse
-              // call.
-              if (callbackEntry.semaphore != null) {
-                callbackEntry.semaphore.release();
-              }
-            }
-          }
-        });
-        t.start();
-      }
-    }
+    JSONObject v8Json = JsonUtil.getAsJSON(json, ChromeDevToolsProtocol.DATA.key);
+    V8CommandProcessor.checkNull(v8Json, "'data' field not found");
+    v8CommandProcessor.processIncomingJson(v8Json);
   }
 
   private void processNavigated(JSONObject json) {
@@ -497,13 +316,26 @@ public class V8DebuggerToolHandler implements ToolHandler {
     return browserImpl.getConnection();
   }
 
-
-  private static boolean isMessageContextDependent(DebuggerMessage message) {
-    DebuggerCommand command = DebuggerCommand.forString(message.getCommand());
-    if (command == null) {
-      return false;
-    }
-    return CONTEXT_DEPENDENT_COMMANDS.contains(command);
+  public V8CommandProcessor getV8CommandProcessor() {
+    return v8CommandProcessor;
   }
-
+  
+  private class ChromeDevToolMessageOutput implements V8CommandOutput {
+    private final String destination;
+   
+    ChromeDevToolMessageOutput(BrowserTabImpl browserTabImpl) {
+      this.destination = String.valueOf(browserTabImpl.getId());
+    }
+   
+    public void send(DebuggerMessage debuggerMessage, boolean isImmediate) {
+      getConnection().send(
+          MessageFactory.debuggerCommand(
+              destination,
+              JsonUtil.streamAwareToJson(debuggerMessage)));
+      if (isImmediate) {
+        getConnection().send(
+            MessageFactory.evaluateJavascript(destination, JAVASCRIPT_VOID));
+      }
+    }
+  }
 }
