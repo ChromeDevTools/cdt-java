34d1beaabee326fd80f5e0711dd3b62d7fe55db3
peter.rybin <peter.rybin@gmail.com>
2009-10-14 17:53:32 +0000

    Make org.chromium.debug.ui compilable in 3.5
    Review URL: http://codereview.chromium.org/273052

--- plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/launcher/LaunchType.java
+++ plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/launcher/LaunchType.java
@@ -11,7 +11,6 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.chromium.debug.core.ChromiumDebugPlugin;
 import org.chromium.debug.core.model.ConnectionLoggerImpl;
 import org.chromium.debug.core.model.ConsolePseudoProcess;
 import org.chromium.debug.core.model.DebugTargetImpl;
@@ -20,33 +19,46 @@ import org.chromium.debug.core.model.DestructingGuard;
 import org.chromium.debug.core.model.JavascriptVmEmbedder;
 import org.chromium.debug.core.model.JavascriptVmEmbedderFactory;
 import org.chromium.debug.core.model.NamedConnectionLoggerFactory;
+import org.chromium.debug.ui.ChromiumDebugUIPlugin;
 import org.chromium.debug.ui.DialogBasedTabSelector;
 import org.chromium.debug.ui.PluginUtil;
 import org.chromium.sdk.ConnectionLogger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.Launch;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
-import org.eclipse.debug.internal.core.LaunchConfiguration;
 
 /**
  * A launch configuration delegate for the JavaScript debugging.
  */
 public abstract class LaunchType implements ILaunchConfigurationDelegate {
 
+  // TODO(peter.rybin): move this class out into a package
   public static class Chromium extends LaunchType {
     @Override
     protected JavascriptVmEmbedder.ConnectionToRemote createConnectionToRemote(int port,
         ILaunch launch, boolean addConsoleLogger) throws CoreException {
       NamedConnectionLoggerFactory consoleFactory =
-        addConsoleLogger ? CONNECTION_LOGGER_FACTORY : NO_CONNECTION_LOGGER_FACTORY;
+          addConsoleLogger ? getLoggerFactory() : NO_CONNECTION_LOGGER_FACTORY;
       return JavascriptVmEmbedderFactory.connectToChromeDevTools(port, consoleFactory,
           new DialogBasedTabSelector());
+    }
+
+    private static NamedConnectionLoggerFactory loggerFactory = null;
+
+    private synchronized static NamedConnectionLoggerFactory getLoggerFactory()
+        throws CoreException {
+      if (loggerFactory == null) {
+        loggerFactory = new ConnectionLoggerFactoryImpl();
+      }
+      return loggerFactory;
     }
 
     /**
@@ -54,25 +66,30 @@ public abstract class LaunchType implements ILaunchConfigurationDelegate {
      * logger console pseudo-projects.
      * TODO(peter.rybin): these projects stay as zombies under the launch; fix it
      */
-    private final static NamedConnectionLoggerFactory CONNECTION_LOGGER_FACTORY =
-        new NamedConnectionLoggerFactory() {
+    private static class ConnectionLoggerFactoryImpl implements NamedConnectionLoggerFactory {
       private final ILaunch commonLaunch;
 
-      {
-        // Let's create configuration first. We might need to create file that holds it.
-        IPath configurationPath;
-        try {
-          configurationPath = initializeConfigurationFile();
-        } catch (IOException e) {
-          throw new RuntimeException("Failed to create configuration file", e); //$NON-NLS-1$
-        }
+      /**
+       * Here we create launch configuration and launch, which will hold console pseudo-process.
+       * This is a bit messy, because ILaunchManager mostly supports user creation of new
+       * configurations in UI.
+       */
+      public ConnectionLoggerFactoryImpl() throws CoreException {
+        String location = Messages.LaunchType_LogConsoleLaunchName + "." + //$NON-NLS-1$
+            ILaunchConfiguration.LAUNCH_CONFIGURATION_FILE_EXTENSION;
+        String memento =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" + //$NON-NLS-1$
+            "<launchConfiguration " + //$NON-NLS-1$
+            "local=\"true\" path=\"" + location + "\"/>"; //$NON-NLS-1$ //$NON-NLS-2$
 
-        LaunchConfiguration configuration = new LaunchConfiguration(configurationPath) {
-          @Override
-          public boolean isLocal() {
-            return true;
-          }
-        };
+        ILaunchConfiguration configuration =
+            DebugPlugin.getDefault().getLaunchManager().getLaunchConfiguration(memento);
+        try {
+          initializeConfigurationFile(configuration.getLocation());
+        } catch (IOException e) {
+          throw new CoreException(new Status(IStatus.ERROR, ChromiumDebugUIPlugin.PLUGIN_ID,
+              "Failed to create launch configuration file", e)); //$NON-NLS-1$
+        }
         commonLaunch = new Launch(configuration, ILaunchManager.DEBUG_MODE, null);
       }
 
@@ -83,10 +100,7 @@ public abstract class LaunchType implements ILaunchConfigurationDelegate {
       /**
        * Creates file so that configuration could be read from some location.
        */
-      private IPath initializeConfigurationFile() throws IOException {
-        IPath configurationPath = ChromiumDebugPlugin.getDefault().getStateLocation().append(
-            Messages.LaunchType_LogConsoleLaunchName
-            + "." + ILaunchConfiguration.LAUNCH_CONFIGURATION_FILE_EXTENSION); //$NON-NLS-1$
+      private void initializeConfigurationFile(IPath configurationPath) throws IOException {
         String osPath = configurationPath.toOSString();
         File file = new File(osPath);
         synchronized (this) {
@@ -97,9 +111,8 @@ public abstract class LaunchType implements ILaunchConfigurationDelegate {
               "type=\"org.chromium.debug.ui.ConsolePseudoConfigurationType\"/>"); //$NON-NLS-1$
           writer.close();
         }
-        return configurationPath;
       }
-    };
+    }
   }
 
   public static class StandaloneV8 extends LaunchType {
