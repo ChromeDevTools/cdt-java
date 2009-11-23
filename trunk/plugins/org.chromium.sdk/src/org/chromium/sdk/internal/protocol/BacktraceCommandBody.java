762d26247d0c01d8ed1a580026176b6bfaf159c1
peter.rybin@gmail.com <peter.rybin@gmail.com@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2009-11-23 13:27:27 +0000

    Add V8 JSON protocol as Java interfaces
    Review URL: http://codereview.chromium.org/434003

---
+++ trunk/plugins/org.chromium.sdk/src/org/chromium/sdk/internal/protocol/BacktraceCommandBody.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.sdk.internal.protocol;
+
+import java.util.List;
+
+import org.chromium.sdk.internal.protocolparser.JsonOptionalField;
+import org.chromium.sdk.internal.protocolparser.JsonSubtype;
+import org.chromium.sdk.internal.protocolparser.JsonType;
+
+@JsonType
+public interface BacktraceCommandBody extends JsonSubtype<CommandResponseBody> {
+
+  List<FrameObject> getFrames();
+
+  @JsonOptionalField
+  Long fromFrame();
+
+  @JsonOptionalField
+  Long toFrame();
+
+  @JsonOptionalField
+  Long totalFrames();
+}
---
+++ trunk/plugins/org.chromium.sdk/src/org/chromium/sdk/internal/protocol/data/ContextData.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.sdk.internal.protocol.data;
+
+import org.chromium.sdk.internal.protocolparser.JsonType;
+
+@JsonType
+public interface ContextData {
+  long value();
+  String type();
+}
