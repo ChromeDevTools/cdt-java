3503d4b54a47c7ce0c453d5a7a4db4a5247d559a
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2010-05-14 16:04:21 +0000

    Add breakpoint properties dialog to "Breakpoints" view
    Review URL: http://codereview.chromium.org/2067005

--- plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/actions/JsBreakpointPropertiesRulerAction.java
+++ plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/actions/JsBreakpointPropertiesRulerAction.java
@@ -7,12 +7,9 @@ package org.chromium.debug.ui.actions;
 import org.chromium.debug.core.model.ChromiumLineBreakpoint;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.ui.actions.RulerBreakpointAction;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.dialogs.PropertyDialogAction;
+import org.eclipse.ui.texteditor.AbstractRulerActionDelegate;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.IUpdate;
 
@@ -31,23 +28,7 @@ public class JsBreakpointPropertiesRulerAction extends RulerBreakpointAction imp
   @Override
   public void run() {
     if (getBreakpoint() != null) {
-      PropertyDialogAction action =
-          new PropertyDialogAction(getEditor().getEditorSite(),
-              new ISelectionProvider() {
-                public void addSelectionChangedListener(ISelectionChangedListener listener) {
-                }
-
-                public ISelection getSelection() {
-                  return new StructuredSelection(getBreakpoint());
-                }
-
-                public void removeSelectionChangedListener(ISelectionChangedListener listener) {
-                }
-
-                public void setSelection(ISelection selection) {
-                }
-              });
-      action.run();
+      JsBreakpointPropertiesAction.runAction(getBreakpoint(), getEditor().getEditorSite());
     }
   }
 
@@ -61,4 +42,13 @@ public class JsBreakpointPropertiesRulerAction extends RulerBreakpointAction imp
     setEnabled(breakpoint != null);
   }
 
+
+  public static class Delegate extends AbstractRulerActionDelegate {
+
+    @Override
+    protected IAction createAction(ITextEditor editor, IVerticalRulerInfo rulerInfo) {
+      return new JsBreakpointPropertiesRulerAction(editor, rulerInfo);
+    }
+
+  }
 }
