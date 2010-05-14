ffc9dc0dd2ca858a5c464b907a73d5dbdd72e59c
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2010-05-14 16:16:11 +0000

    Refactor UI <-> SDK breakpoints mapping
    Review URL: http://codereview.chromium.org/2113001

--- plugins/org.chromium.debug.core/src/org/chromium/debug/core/model/BreakpointMap.java
+++ plugins/org.chromium.debug.core/src/org/chromium/debug/core/model/BreakpointMap.java
@@ -4,77 +4,35 @@
 
 package org.chromium.debug.core.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.chromium.sdk.Breakpoint;
 
 /**
- * A map between breakpoints in ChromeDevTools SDK and in Eclipse debugger framework.
+ * TODO(peter.rybin): this class is obsolete, it only holds useful inner class;
+ * consider removing this class.
  */
 public class BreakpointMap {
 
   /**
-   * For Eclipse UI breakpoint returns all corresponding breakpoints in SDK. Since several debug
-   * launches/targets may be opened for the same set of source files, one UI breakpoint may
-   * correspond to several SDK breakpoints in different debug launches/targets.
-   */
-  public Collection<BreakpointMapping> getSdkBreakpoints(
-      ChromiumLineBreakpoint chromiumLineBreakpoint) {
-    List<BreakpointMapping> res = new ArrayList<BreakpointMapping>(inTargetMaps.size());
-    for (InTargetMap map : inTargetMaps) {
-      Breakpoint sdkBreakpoint = map.getSdkBreakpoint(chromiumLineBreakpoint);
-      if (sdkBreakpoint == null) {
-        continue;
-      }
-      res.add(new BreakpointMapping(sdkBreakpoint, map.getDebugTarget()));
-    }
-    return res;
-  }
-
-  public InTargetMap addTarget(DebugTargetImpl debugTarget) {
-    InTargetMap inTargetMap = new InTargetMap(debugTarget);
-    inTargetMaps.add(inTargetMap);
-    return inTargetMap;
-  }
-
-  private final List<InTargetMap> inTargetMaps = new ArrayList<InTargetMap>(3);
-
-
-  /**
    * A one-to-one map between SDK and UI breakpoints inside one debug target.
    */
-  public class InTargetMap {
-    private final DebugTargetImpl debugTarget;
+  public static class InTargetMap {
     private final Map<Breakpoint, ChromiumLineBreakpoint> sdkToUiMap =
         new HashMap<Breakpoint, ChromiumLineBreakpoint>();
     private final Map<ChromiumLineBreakpoint, Breakpoint> uiToSdkMap =
       new HashMap<ChromiumLineBreakpoint, Breakpoint>();
 
-    private InTargetMap(DebugTargetImpl debugTarget) {
-      this.debugTarget = debugTarget;
+    public InTargetMap() {
     }
 
-    private Breakpoint getSdkBreakpoint(ChromiumLineBreakpoint chromiumLineBreakpoint) {
+    public Breakpoint getSdkBreakpoint(ChromiumLineBreakpoint chromiumLineBreakpoint) {
       return uiToSdkMap.get(chromiumLineBreakpoint);
     }
 
     public ChromiumLineBreakpoint getUiBreakpoint(Breakpoint sdkBreakpoint) {
       return sdkToUiMap.get(sdkBreakpoint);
-    }
-
-    public DebugTargetImpl getDebugTarget() {
-      return debugTarget;
-    }
-
-    public void dispose() {
-      boolean res = inTargetMaps.remove(this);
-      if (!res) {
-        throw new IllegalStateException();
-      }
     }
 
     public void add(Breakpoint sdkBreakpoint, ChromiumLineBreakpoint uiBreakpoint) {
@@ -91,24 +49,6 @@ public class BreakpointMap {
         throw new RuntimeException();
       }
       sdkToUiMap.remove(sdkBreakpoint);
-    }
-  }
-
-  public static class BreakpointMapping {
-    private final Breakpoint sdkBreakpoint;
-    private final DebugTargetImpl debugTargetImpl;
-
-    BreakpointMapping(Breakpoint sdkBreakpoint, DebugTargetImpl debugTargetImpl) {
-      this.sdkBreakpoint = sdkBreakpoint;
-      this.debugTargetImpl = debugTargetImpl;
-    }
-
-    public Breakpoint getSdkBreakpoint() {
-      return sdkBreakpoint;
-    }
-
-    public DebugTargetImpl getDebugTarget() {
-      return debugTargetImpl;
     }
   }
 }
