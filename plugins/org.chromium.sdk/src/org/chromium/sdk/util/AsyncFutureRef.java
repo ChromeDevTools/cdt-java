ed2a8b69fc16cd473b83a74c3242c69bfa2d8a10
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-03-09 17:01:16 +0000

    AsyncFuture
    Review URL: http://codereview.chromium.org/6588024

---
+++ plugins/org.chromium.sdk/src/org/chromium/sdk/util/AsyncFutureRef.java
@@ -0,0 +1,43 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.sdk.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.chromium.sdk.SyncCallback;
+import org.chromium.sdk.util.AsyncFuture;
+import org.chromium.sdk.util.AsyncFuture.Operation;
+
+/**
+ * A wrapper around {@link AtomicReference} and {@link AsyncFuture} that makes the source code
+ * cleaner and shorter.
+ */
+public class AsyncFutureRef<T> {
+  private final AtomicReference<AsyncFuture<T>> ref = new AtomicReference<AsyncFuture<T>>(null);
+
+  public void initializeRunning(Operation<T> requester) {
+    AsyncFuture.initializeReference(ref, requester);
+  }
+
+  public void initializeTrivial(T value) {
+    AsyncFuture.initializeTrivial(ref, value);
+  }
+
+  public boolean isInitialized() {
+    return ref.get() != null;
+  }
+
+  public T getSync() {
+    return ref.get().getSync();
+  }
+
+  public void getAsync(AsyncFuture.Callback<T> callback, SyncCallback syncCallback) {
+    ref.get().getAsync(callback, syncCallback);
+  }
+
+  public boolean isDone() {
+    return ref.get().isDone();
+  }
+}
