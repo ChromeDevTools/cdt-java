a26967e6c9e607ef37f2713b0a3ac07087cf7cd1
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-03-09 17:38:51 +0000

    Rynda protocol interfaces
    Review URL: http://codereview.chromium.org/6538099

---
+++ plugins/org.chromium.sdk/src-rynda/org/chromium/sdk/internal/rynda/protocol/input/SetBreakpointData.java
@@ -0,0 +1,17 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.sdk.internal.rynda.protocol.input;
+
+import org.chromium.sdk.internal.protocolparser.JsonSubtype;
+import org.chromium.sdk.internal.protocolparser.JsonType;
+
+@JsonType
+public interface SetBreakpointData extends JsonSubtype<RyndaCommandResponse.Data> {
+
+  String breakpointId();
+
+  // Unspecified here.
+  Object locations();
+}
