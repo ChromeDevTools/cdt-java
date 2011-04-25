5a879df44fd34f7faf8dfe59365ca0c9c7638fb6
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-04-25 22:02:02 +0000

    Support 'enabled' for breakpoints and fix minor bugs
    Review URL: http://codereview.chromium.org/6900034

--- plugins/org.chromium.sdk/src-wip/org/chromium/sdk/internal/wip/WipBreakpointImpl.java
+++ plugins/org.chromium.sdk/src-wip/org/chromium/sdk/internal/wip/WipBreakpointImpl.java
@@ -11,6 +11,7 @@ import org.chromium.sdk.JavascriptVm.GenericCallback;
 import org.chromium.sdk.SyncCallback;
 import org.chromium.sdk.internal.wip.protocol.input.WipCommandResponse.Success;
 import org.chromium.sdk.internal.wip.protocol.input.debugger.SetBreakpointByUrlData;
+import org.chromium.sdk.internal.wip.protocol.output.debugger.EnableParams;
 import org.chromium.sdk.internal.wip.protocol.output.debugger.RemoveBreakpointParams;
 import org.chromium.sdk.internal.wip.protocol.output.debugger.SetBreakpointByUrlParams;
 import org.chromium.sdk.util.Destructable;
@@ -19,6 +20,8 @@ import org.chromium.sdk.util.DestructingGuard;
 
 /**
  * Wip-based breakpoint implementation.
+ * The implementation is based on volatile fields and expects client code to do some
+ * synchronization (serialize calls to setters, {@link #flush} and {@link #clear}).
  */
 public class WipBreakpointImpl implements Breakpoint {
   private final WipTabImpl tabImpl;
@@ -29,18 +32,16 @@ public class WipBreakpointImpl implements Breakpoint {
   private final int columnNumber;
 
   private final int sdkId;
-  private volatile String protocolId;
+  private volatile String protocolId = null;
 
   private volatile String condition;
   private volatile boolean enabled;
   private volatile boolean isDirty;
 
-  public WipBreakpointImpl(WipTabImpl tabImpl, int sdkId, String protocolId,
-      ScriptUrlOrId script, int lineNumber, int columnNumber,
-      String condition, boolean enabled) {
+  public WipBreakpointImpl(WipTabImpl tabImpl, int sdkId, ScriptUrlOrId script,
+      int lineNumber, int columnNumber, String condition, boolean enabled) {
     this.tabImpl = tabImpl;
     this.sdkId = sdkId;
-    this.protocolId = protocolId;
     this.script = script;
     this.lineNumber = lineNumber;
     this.columnNumber = columnNumber;
@@ -112,6 +113,9 @@ public class WipBreakpointImpl implements Breakpoint {
 
   @Override
   public void setEnabled(boolean enabled) {
+    if (enabled == this.enabled) {
+      return;
+    }
     this.enabled = enabled;
     isDirty  = true;
   }
@@ -134,12 +138,24 @@ public class WipBreakpointImpl implements Breakpoint {
 
   @Override
   public void setCondition(String condition) {
+    if (eq(this.condition, condition)) {
+      return;
+    }
     this.condition = condition;
     isDirty = true;
   }
 
+  void setProtocolId(String protocolId) {
+    this.protocolId = protocolId;
+  }
+
   @Override
   public void clear(final BreakpointCallback callback, SyncCallback syncCallback) {
+    if (protocolId == null) {
+      callback.success(this);
+      syncCallback.callbackDone(null);
+    }
+
     RemoveBreakpointParams params = new RemoveBreakpointParams(protocolId);
 
     WipCommandCallback commandCallback;
@@ -161,68 +177,76 @@ public class WipBreakpointImpl implements Breakpoint {
 
   @Override
   public void flush(final BreakpointCallback callback, final SyncCallback syncCallback) {
-    {
-      // Is it thread-safe-enough for us?
-      if (!isDirty) {
-        return;
-      }
+    if (!isDirty) {
+      return;
     }
     isDirty = false;
 
     final Destructable callbackAsDestructable =
         DestructableWrapper.callbackAsDestructable(syncCallback);
 
-    // Call syncCallback if something goes wrong after we sent request.
-    final DestructingGuard guard = new DestructingGuard();
+    if (protocolId == null) {
+      // Breakpoint was disabled, it doesn't exist in VM, immediately start step 2.
+      recreateBreakpointAsync(callback, callbackAsDestructable);
+    } else {
+      // Call syncCallback if something goes wrong after we sent request.
+      final DestructingGuard guard = new DestructingGuard();
 
-    WipCommandCallback removeCallback = new WipCommandCallback.Default() {
-      @Override
-      protected void onSuccess(Success success) {
-        recreateBreakpointAsync(callback, callbackAsDestructable);
-        guard.discharge();
-      }
+      WipCommandCallback removeCallback = new WipCommandCallback.Default() {
+        @Override
+        protected void onSuccess(Success success) {
+          setProtocolId(null);
+          recreateBreakpointAsync(callback, callbackAsDestructable);
+          guard.discharge();
+        }
 
-      @Override
-      protected void onError(String message) {
-        throw new RuntimeException("Failed to remove breakpoint: " + message);
-      }
-    };
+        @Override
+        protected void onError(String message) {
+          throw new RuntimeException("Failed to remove breakpoint: " + message);
+        }
+      };
 
-    // Call syncCallback if something goes wrong.
-    guard.addValue(callbackAsDestructable);
-    tabImpl.getCommandProcessor().send(new RemoveBreakpointParams(protocolId), removeCallback,
-        DestructableWrapper.guardAsCallback(guard));
+      // Call syncCallback if something goes wrong.
+      guard.addValue(callbackAsDestructable);
+      tabImpl.getCommandProcessor().send(new RemoveBreakpointParams(protocolId), removeCallback,
+          DestructableWrapper.guardAsCallback(guard));
+    }
   }
 
   private void recreateBreakpointAsync(final BreakpointCallback flushCallback,
       Destructable callbackAsDestructable) {
-    GenericCallback<String> setCommandCallback = new GenericCallback<String>() {
-      @Override
-      public void success(String protocolId) {
-        WipBreakpointImpl.this.protocolId = protocolId;
-        if (flushCallback != null) {
-          flushCallback.success(WipBreakpointImpl.this);
+
+    if (enabled) {
+      GenericCallback<String> setCommandCallback = new GenericCallback<String>() {
+        @Override
+        public void success(String protocolId) {
+          setProtocolId(protocolId);
+          if (flushCallback != null) {
+            flushCallback.success(WipBreakpointImpl.this);
+          }
         }
-      }
 
-      @Override
-      public void failure(Exception exception) {
-        if (flushCallback != null) {
-          flushCallback.failure(exception.getMessage());
+        @Override
+        public void failure(Exception exception) {
+          if (flushCallback != null) {
+            flushCallback.failure(exception.getMessage());
+          }
         }
+      };
+
+      DestructingGuard guard = new DestructingGuard();
+      guard.addValue(callbackAsDestructable);
+
+      if (condition == null) {
+        condition = "";
       }
-    };
-
-    DestructingGuard guard = new DestructingGuard();
-    guard.addValue(callbackAsDestructable);
-
-    if (condition == null) {
-      condition = "";
+      sendSetBreakpointRequest(script, lineNumber, columnNumber, condition,
+          setCommandCallback, DestructableWrapper.guardAsCallback(guard),
+          tabImpl.getCommandProcessor());
+    } else {
+      // Breakpoint is disabled, do not create it.
+      callbackAsDestructable.destruct();
     }
-    // TODO: support enabled.
-    sendSetBreakpointRequest(script, lineNumber, columnNumber, condition,
-        setCommandCallback, DestructableWrapper.guardAsCallback(guard),
-        tabImpl.getCommandProcessor());
   }
 
   /**
@@ -241,7 +265,6 @@ public class WipBreakpointImpl implements Breakpoint {
 
       @Override
       public Void forUrl(String url) {
-        // TODO: support enabled.
         SetBreakpointByUrlParams request =
             new SetBreakpointByUrlParams(url, lineNumber, (long) columnNumber, condition);
 
@@ -266,5 +289,9 @@ public class WipBreakpointImpl implements Breakpoint {
         return null;
       }
     });
+  }
+
+  private static <T> boolean eq(T left, T right) {
+    return left == right || (left != null && left.equals(right));
   }
 }
