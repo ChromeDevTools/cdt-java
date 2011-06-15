072d7b3ffa9495e86301d828ba12125ea792b7ec
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-06-15 23:29:06 +0000

    Provide missing 'isJsFile' property tester for IEditorInput
    Review URL: http://codereview.chromium.org/7104124

--- plugins/org.chromium.debug.core/src/org/chromium/debug/core/FilePropertyTester.java
+++ plugins/org.chromium.debug.core/src/org/chromium/debug/core/FilePropertyTester.java
@@ -11,17 +11,26 @@ import org.eclipse.core.resources.IFile;
 /**
  * Implementation of additional properties for files that are JavaScript sources.
  */
-public class FilePropertyTester extends PropertyTester {
+public abstract class FilePropertyTester extends PropertyTester {
   private static final String IS_JS_FILE_PROPERTY = "isJsFile"; //$NON-NLS-1$
 
+  public static class ForFile extends FilePropertyTester {
+    @Override
+    protected IFile extractFile(Object receiver) {
+      return (IFile) receiver;
+    }
+  }
+
   public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-    IFile file = (IFile) receiver;
+    IFile file = extractFile(receiver);
     if (IS_JS_FILE_PROPERTY.equals(property)) {
       return testIsJsFile(file);
     } else {
       throw new RuntimeException("Unrecognized property name"); //$NON-NLS-1$
     }
   }
+
+  protected abstract IFile extractFile(Object receiver);
 
   private static boolean testIsJsFile(IFile file) {
     return ChromiumDebugPluginUtil.SUPPORTED_EXTENSIONS.contains(file.getFileExtension());
