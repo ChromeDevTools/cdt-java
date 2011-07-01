1bdecffeeb8f5c4efe3cbf79438894d703ea4dfe
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-07-01 18:59:26 +0000

    Formalize remote-shell-port protocol JSON
    Review URL: http://codereview.chromium.org/7273083

--- plugins/org.chromium.sdk/src/org/chromium/sdk/internal/shellprotocol/tools/protocol/output/MessageFactory.java
+++ plugins/org.chromium.sdk/src/org/chromium/sdk/internal/shellprotocol/tools/protocol/output/MessageFactory.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.sdk.internal.shellprotocol.tools;
+package org.chromium.sdk.internal.shellprotocol.tools.protocol.output;
 
 import java.util.HashMap;
 import java.util.Map;
