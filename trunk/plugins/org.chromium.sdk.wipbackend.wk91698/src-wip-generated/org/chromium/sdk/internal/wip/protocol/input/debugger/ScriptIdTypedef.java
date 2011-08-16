ce48b232d4138086d2cc457f5454f26fa92edc71
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-08-16 18:08:50 +0000

    branch wip backend as wk91698

---
+++ trunk/plugins/org.chromium.sdk.wipbackend.wk91698/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/debugger/ScriptIdTypedef.java
@@ -0,0 +1,10 @@
+// Generated source.
+// Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
+// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@92377
+
+package org.chromium.sdk.internal.wip.protocol.input.debugger;
+
+/**
+ Unique script identifier.
+ */
+public class ScriptIdTypedef {/*Typedef class, merely holds a javadoc.*/}
