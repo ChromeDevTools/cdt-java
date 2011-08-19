9f6301aa3a4f822e53770e00f0ebc2dac7e7d06f
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-08-19 00:27:53 +0000

    Add small fixes before release
    Review URL: http://codereview.chromium.org/7631060

---
+++ plugins/org.chromium.sdk/src/org/chromium/sdk/package-info.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Main package of ChromDevTools SDK API.
+ * See Tutorial at http://code.google.com/p/chromedevtools/wiki/SdkTutorial
+ */
+package org.chromium.sdk;
