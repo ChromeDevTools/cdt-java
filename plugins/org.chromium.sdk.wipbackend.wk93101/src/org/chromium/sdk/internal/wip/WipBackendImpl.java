53523d9afc9933740d95614c2f7df4d837dbe213
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-10-17 23:12:10 +0000

    Prepare new WIP backend
    Review URL: http://codereview.chromium.org/8333002

--- plugins/org.chromium.sdk.wipbackend.wk93101/src/org/chromium/sdk/internal/wip/WipBackendImpl.java
+++ plugins/org.chromium.sdk.wipbackend.wk93101/src/org/chromium/sdk/internal/wip/WipBackendImpl.java
@@ -32,9 +32,9 @@ public class WipBackendImpl extends WipBackendBase {
 
   private static final String ID = "WK@93101";
   private static final String DESCRIPTION =
-      "Google Chrome/Chromium: 15.0.855.*\n" +
-      "Chromium build: 97053\n" +
-      "WebKit revision: 93101\n";
+      "Google Chrome/Chromium: 15.0.874.*\n" +
+      "Chromium build: 97053, 99889\n" +
+      "WebKit revision: 93101, 94560\n";
 
   public WipBackendImpl() {
     super(ID, DESCRIPTION);
