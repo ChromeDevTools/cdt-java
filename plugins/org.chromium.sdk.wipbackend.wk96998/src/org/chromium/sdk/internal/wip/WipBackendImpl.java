0a7b3aa3159500ea380445d20d8c52b4f8232f65
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2011-10-27 16:05:30 +0000

    Rename backend
    Review URL: http://codereview.chromium.org/8402029

--- plugins/org.chromium.sdk.wipbackend.wk96998/src/org/chromium/sdk/internal/wip/WipBackendImpl.java
+++ plugins/org.chromium.sdk.wipbackend.wk96998/src/org/chromium/sdk/internal/wip/WipBackendImpl.java
@@ -30,11 +30,11 @@ import org.json.simple.parser.ParseException;
 public class WipBackendImpl extends WipBackendBase {
   private static final int DEFAULT_CONNECTION_TIMEOUT_MS = 1000;
 
-  private static final String ID = "WK@96998";
+  private static final String ID = "WK@97678";
   private static final String DESCRIPTION =
-      "Google Chrome/Chromium: 16.0.904.*\n" +
-      "Chromium build: 104662\n" +
-      "WebKit revision: 96998\n";
+      "Google Chrome/Chromium: 16.0.912.*\n" +
+      "Chromium build: 106036\n" +
+      "WebKit revision: 97678\n";
 
   public WipBackendImpl() {
     super(ID, DESCRIPTION);
