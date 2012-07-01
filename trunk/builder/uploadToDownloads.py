85b5678881bf5ee674ba1480645bf5f7ea298e01
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2012-07-01 22:07:45 +0000

    Add comments to build script
    Review URL: https://chromiumcodereview.appspot.com/10690061

--- trunk/builder/uploadToDownloads.py
+++ trunk/builder/uploadToDownloads.py
@@ -2,6 +2,8 @@
 #  Use of this source code is governed by a BSD-style license that can be
 #  found in the LICENSE file.
 
+# This script is expected to be called from build.xml uploadToDownloads target
+
 import sys
 import getpass
 import googlecode_upload
