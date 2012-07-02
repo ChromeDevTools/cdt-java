3f51ef3a3fae718e3ff2973482d5f8d458c556ed
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2012-07-02 16:51:02 +0000

    Don't auto-wrap files from virtual project and drop empty action set
    Review URL: https://chromiumcodereview.appspot.com/10695053

--- trunk/plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/propertypages/ScriptFilePage.java
+++ trunk/plugins/org.chromium.debug.ui/src/org/chromium/debug/ui/propertypages/ScriptFilePage.java
@@ -69,6 +69,8 @@ public class ScriptFilePage extends PropertyPage {
   }
 
   private boolean isVProjectFile(IFile file, ScriptTargetMapping mapping) {
+    // TODO: Consider calling mapping.isVirtualProjectResource() instead.
+
     IProject project = file.getProject();
     if (project == null) {
       return false;
