b314a0d4524adc59b095df9f0f7bb39fb809ed4f
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2012-11-26 20:57:04 +0000

    Regenerate interfaces to support open property set
    Review URL: https://codereview.chromium.org/11316177

--- plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/dom/DocumentUpdatedEventData.java
+++ plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/dom/DocumentUpdatedEventData.java
@@ -1,14 +1,14 @@
 // Generated source.
 // Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
-// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@96703
+// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@135591
 
 package org.chromium.sdk.internal.wip.protocol.input.dom;
 
 /**
  Fired when <code>Document</code> has been totally updated. Node ids are no longer valid.
  */
-@org.chromium.sdk.internal.protocolparser.JsonType
-public interface DocumentUpdatedEventData {
+@org.chromium.sdk.internal.protocolparser.JsonType(allowsOtherProperties=true)
+public interface DocumentUpdatedEventData extends org.chromium.sdk.internal.protocolparser.JsonObjectBased {
   public static final org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.dom.DocumentUpdatedEventData> TYPE
       = new org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.dom.DocumentUpdatedEventData>("DOM.documentUpdated", org.chromium.sdk.internal.wip.protocol.input.dom.DocumentUpdatedEventData.class) {
     @Override public org.chromium.sdk.internal.wip.protocol.input.dom.DocumentUpdatedEventData parse(org.chromium.sdk.internal.wip.protocol.input.WipGeneratedParserRoot parser, org.json.simple.JSONObject obj) throws org.chromium.sdk.internal.protocolparser.JsonProtocolParseException {
--- plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/network/HeadersValue.java
+++ plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/network/HeadersValue.java
@@ -1,12 +1,12 @@
 // Generated source.
 // Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
-// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@96703
+// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@135591
 
 package org.chromium.sdk.internal.wip.protocol.input.network;
 
 /**
  Request / response headers as keys / values of JSON object.
  */
-@org.chromium.sdk.internal.protocolparser.JsonType
-public interface HeadersValue {
+@org.chromium.sdk.internal.protocolparser.JsonType(allowsOtherProperties=true)
+public interface HeadersValue extends org.chromium.sdk.internal.protocolparser.JsonObjectBased {
 }
