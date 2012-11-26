b3da10b8489326eedbeffc8125362d1e215a92cf
peter.rybin <peter.rybin@fc8a088e-31da-11de-8fef-1f5ae417a2df>
2012-11-26 20:57:04 +0000

    Regenerate interfaces to support open property set
    Review URL: https://codereview.chromium.org/11316177

--- trunk/plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/debugger/GlobalObjectClearedEventData.java
+++ trunk/plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/debugger/GlobalObjectClearedEventData.java
@@ -1,14 +1,14 @@
 // Generated source.
 // Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
-// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@101756
+// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@135591
 
 package org.chromium.sdk.internal.wip.protocol.input.debugger;
 
 /**
  Called when global has been cleared and debugger client should reset its state. Happens upon navigation or reload.
  */
-@org.chromium.sdk.internal.protocolparser.JsonType
-public interface GlobalObjectClearedEventData {
+@org.chromium.sdk.internal.protocolparser.JsonType(allowsOtherProperties=true)
+public interface GlobalObjectClearedEventData extends org.chromium.sdk.internal.protocolparser.JsonObjectBased {
   public static final org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.debugger.GlobalObjectClearedEventData> TYPE
       = new org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.debugger.GlobalObjectClearedEventData>("Debugger.globalObjectCleared", org.chromium.sdk.internal.wip.protocol.input.debugger.GlobalObjectClearedEventData.class) {
     @Override public org.chromium.sdk.internal.wip.protocol.input.debugger.GlobalObjectClearedEventData parse(org.chromium.sdk.internal.wip.protocol.input.WipGeneratedParserRoot parser, org.json.simple.JSONObject obj) throws org.chromium.sdk.internal.protocolparser.JsonProtocolParseException {
--- trunk/plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/debugger/ResumedEventData.java
+++ trunk/plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/input/debugger/ResumedEventData.java
@@ -1,14 +1,14 @@
 // Generated source.
 // Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
-// Origin: http://svn.webkit.org/repository/webkit/!svn/bc/92284/trunk/Source/WebCore/inspector/Inspector.json@92284
+// Origin: http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/inspector/Inspector.json@135591
 
 package org.chromium.sdk.internal.wip.protocol.input.debugger;
 
 /**
  Fired when the virtual machine resumed execution.
  */
-@org.chromium.sdk.internal.protocolparser.JsonType
-public interface ResumedEventData {
+@org.chromium.sdk.internal.protocolparser.JsonType(allowsOtherProperties=true)
+public interface ResumedEventData extends org.chromium.sdk.internal.protocolparser.JsonObjectBased {
   public static final org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.debugger.ResumedEventData> TYPE
       = new org.chromium.sdk.internal.wip.protocol.input.WipEventType<org.chromium.sdk.internal.wip.protocol.input.debugger.ResumedEventData>("Debugger.resumed", org.chromium.sdk.internal.wip.protocol.input.debugger.ResumedEventData.class) {
     @Override public org.chromium.sdk.internal.wip.protocol.input.debugger.ResumedEventData parse(org.chromium.sdk.internal.wip.protocol.input.WipGeneratedParserRoot parser, org.json.simple.JSONObject obj) throws org.chromium.sdk.internal.protocolparser.JsonProtocolParseException {
