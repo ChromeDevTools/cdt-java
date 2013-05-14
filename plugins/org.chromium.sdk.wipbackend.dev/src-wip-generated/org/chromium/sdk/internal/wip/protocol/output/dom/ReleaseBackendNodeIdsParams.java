a34b3e732b36f48f9c7715bca0aff02a01118fb5
peter.rybin <peter.rybin@gmail.com>
2013-05-14 16:13:19 +0000

    Fix generated annotations
    Review URL: https://codereview.chromium.org/15169003

--- plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/output/dom/ReleaseBackendNodeIdsParams.java
+++ plugins/org.chromium.sdk.wipbackend.dev/src-wip-generated/org/chromium/sdk/internal/wip/protocol/output/dom/ReleaseBackendNodeIdsParams.java
@@ -1,6 +1,6 @@
 // Generated source.
 // Generator: org.chromium.sdk.internal.wip.tools.protocolgenerator.Generator
-// Origin: http://src.chromium.org/blink/trunk/Source/devtools/protocol.json@<unknown>
+// Origin: http://src.chromium.org/blink/trunk/Source/devtools/protocol.json@150309 with change #14672031
 
 package org.chromium.sdk.internal.wip.protocol.output.dom;
 
